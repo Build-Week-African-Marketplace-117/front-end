49ce2a4fc48f74193ed97a2155d86e2fc79fb4c1
Lakendria <kendra939@gmail.com>
2021-01-28 22:31:11 -0500

    routes working

--- crud-app/src/App.js
+++ crud-app/src/App.js
@@ -2,17 +2,23 @@
 import './App.css';
 import MarketList from './components/MarketList';
 import Home from './pages/Home';
+import {BrowserRouter as Router,Route} from 'react-router-dom';
+import {PrivateRoute} from '../src/utils/PrivateRoute';
+
 
 function App() {
   return (
+    <Router>
     <div className="App">
-     <h1>African Marketplace</h1>
-     <Home/>
-   
+   <Route exact path ="/" component={Home}/>
+   <PrivateRoute exact path = "/protected">
      <MarketList/>
+     </PrivateRoute>
     
     </div>
+    </Router>
   );
 }
+  
 
 export default App;
