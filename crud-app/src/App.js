bc7a9cd9aaf0d675f5b909be1a935b356318f3f2
Lakendria <kendra939@gmail.com>
2021-01-28 20:46:37 -0500

    home component created

--- crud-app/src/App.js
+++ crud-app/src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
 import MarketList from './components/MarketList';
+import Home from './pages/Home';
 
 function App() {
   return (
     <div className="App">
      <h1>African Marketplace</h1>
+     <Home/>
    
      <MarketList/>
     
