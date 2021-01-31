bd6bfccddae9d8a75b1593f3479a922de94e91ab
Lakendria <kendra939@gmail.com>
2021-01-31 15:53:29 -0500

    merged-login and signup

--- crud-app/src/App.js
+++ crud-app/src/App.js
@@ -1,7 +1,9 @@
 
-import './App.css';
+//import './App.css';
 import MarketList from './components/MarketList';
 import Home from './pages/Home';
+import SignUp from './Conponents/SignUp';
+import SignIn from './Conponents/SignIn';
 import {BrowserRouter as Router,Route} from 'react-router-dom';
 import {PrivateRoute} from '../src/utils/PrivateRoute';
 
@@ -11,6 +13,8 @@ function App() {
     <Router>
     <div className="App">
    <Route exact path ="/" component={Home}/>
+   <Route exact path ="/sign-in" component={SignIn}/>
+   <Route exact path ="/sign-up" component={SignUp}/>
    <PrivateRoute exact path = "/protected">
      <MarketList/>
      </PrivateRoute>
